a90f348d478589d03288a774ceec5b9b61364800
ozioma45 <oziomaegole@gmail.com>
2024-02-16 05:51:03 +0100

    Dynamic Values in Templates

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const title = "welcomw to the new blog";
+  const likes = 50;
+  /* const person = { name: "ozioma", age: 30 }; */
+  const link = "www.google.com";
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="content">
+        <h1>App Component</h1>
+        <h1>{title}</h1>
+        <p>Likes {likes} times</p>
+        {/*  <p>{person}</p> */}
+
+        <p>{10}</p>
+        <p>{"Hello world"}</p>
+        <p>{[1, 2, 3, 4, 5]}</p>
+        <p>{Math.random() * 10}</p>
+
+        <a href={link}>Google link</a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
