fc5220f15c6a1f0024137c704199f3cc41a1b9b5
ozioma45 <oziomaegole@gmail.com>
2024-02-18 20:31:40 +0100

    using state (useState hook)

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,14 @@
+import { useState } from "react";
+
 export default function Home() {
+  //let name = "Mario";
+
+  const [name, setName] = useState("Mario");
+  const [Age, setAge] = useState("23");
+
   const handleClick = () => {
-    console.log("Hello World");
+    setName("John");
+    setAge(30);
   };
 
   const handleClickAgain = (name) => {
@@ -11,6 +19,9 @@ export default function Home() {
     <div className="Home">
       <h2>Homepage</h2>
       <button onClick={handleClick}>Click me</button>
+      <p>
+        {name} is {Age} years old
+      </p>
       <button
         onClick={() => {
           handleClickAgain("Mario");
