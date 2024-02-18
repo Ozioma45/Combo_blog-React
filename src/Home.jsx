2bff58ff0ceffd71c0b7a2838af4e72c740b770f
ozioma45 <oziomaegole@gmail.com>
2024-02-18 20:17:55 +0100

    click events and functions

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,22 @@
 export default function Home() {
+  const handleClick = () => {
+    console.log("Hello World");
+  };
+
+  const handleClickAgain = (name) => {
+    console.log("hello " + name);
+  };
+
   return (
     <div className="Home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button
+        onClick={() => {
+          handleClickAgain("Mario");
+        }}>
+        Click me again
+      </button>
     </div>
   );
 }
