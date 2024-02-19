790d954eec8df27584a2d46a190577e4dddf46ca
ozioma45 <oziomaegole@gmail.com>
2024-02-19 03:11:41 +0100

    reusing props

--- src/Home.jsx
+++ src/Home.jsx
@@ -15,6 +15,10 @@ export default function Home() {
   return (
     <div className="Home">
       <BlogList blogs={blogs} title="All Blogs" />
+      <BlogList
+        blogs={blogs.filter((blogs) => blogs.author === "mario")}
+        title="mario'S Blogs"
+      />
     </div>
   );
 }
