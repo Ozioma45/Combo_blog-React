35e50c3ca6f144a6dbddd2cb70690973a8e2a4dc
Ozioma45 <oziomaegole@gmail.com>
2024-02-26 03:32:54 +0100

    controlled inputs(forms)

--- src/Create.jsx
+++ src/Create.jsx
@@ -1,7 +1,41 @@
+import { useState } from "react";
+
 export default function Create() {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("Mario");
+
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
+      <form>
+        <label>Blog Title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <label>Blog Body:</label>
+        <textarea
+          type="text"
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}></textarea>
+
+        <label>Blog Author</label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="Mario">Mario</option>
+          <option value="John">John</option>
+        </select>
+
+        <button>Add Blog</button>
+      </form>
+
+      <p>{title}</p>
+      <p>{body}</p>
+      <p>{author}</p>
     </div>
   );
 }
